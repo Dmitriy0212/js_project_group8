0ccddd20874b71095537fc0a5f7f9f8822549a7f
Dmitriy0212 <demarr68@gmail.com>
2026-04-02 14:40:59 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: command === 'serve' ? '/' : '/js_project_group8/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
