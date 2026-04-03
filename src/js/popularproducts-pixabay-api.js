63583d90a662b12544b32e30cae251cb65b2b84f
Dmitriy0212 <demarr68@gmail.com>
2026-04-03 19:29:28 +0300

    bug fixes

---
+++ src/js/popularproducts-pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { BASE_URL } from './baseUrl';
+export async function getPopularProducts() {
+  try {
+    const response = await axios.get(`${BASE_URL}furnitures`, {
+      params: { type: 'popular' },
+    });
+
+    return response.data.furnitures;
+  } catch (error) {
+    console.error(
+      'Ошибка при запросе:',
+      error.response?.status,
+      error.response?.data
+    );
+    return null;
+  }
+}
--- src/js/popularproducts.js
+++ src/js/popularproducts.js
@@ -1,52 +1,89 @@
-/*export default async function initSwiper() {
-  // Данные слайдов
-  const slidesData = [
-    { img: 'https://picsum.photos/300/200?1', title: 'Слайд 1' },
-    { img: 'https://picsum.photos/300/200?2', title: 'Слайд 2' },
-    { img: 'https://picsum.photos/300/200?3', title: 'Слайд 3' },
-    { img: 'https://picsum.photos/300/200?4', title: 'Слайд 4' },
-    { img: 'https://picsum.photos/300/200?5', title: 'Слайд 5' },
-  ];
+import Swiper from 'swiper/bundle';
+import 'swiper/css';
+import { getPopularProducts } from './popularproducts-pixabay-api.js';
 
-  const slidesList = document.getElementById('slidesList');
+export default async function initSwiper() {
+  const sliderWrapper = document.querySelector('.slider__wrapper');
+  if (!sliderWrapper) {
+    console.error('sliderWrapper не найден!');
+    return;
+  }
 
-  // Создаём li для каждого слайда
-  slidesList.innerHTML = slidesData
-    .map(
-      slide => `
-    <li class="swiper-slide">
-      <img src="${slide.img}" alt="${slide.title}">
-      <h3>${slide.title}</h3>
-    </li>
-  `
-    )
-    .join('');
+  let slidesData = [];
+  try {
+    slidesData = await getPopularProducts();
+  } catch (error) {
+    console.log('Помилка в doStuff:', error);
+  }
 
-  // Инициализация Swiper
-  const swiper = new Swiper('.mySwiper', {
-    slidesPerView: 3, // Показывать 3 слайда одновременно
-    slidesPerGroup: 3, // Листать по 3 слайда
-    spaceBetween: 20, // Отступ между слайдами
-    pagination: { el: '.swiper-pagination', clickable: true },
+  if (!Array.isArray(slidesData)) slidesData = [];
+  debugger;
+
+  sliderWrapper.innerHTML = a(slidesData);
+
+  const swiper = new Swiper('.slider', {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 20,
+    pagination: {
+      el: '.slider__pagination',
+      clickable: true,
+      bulletClass: 'slider__bullet',
+      bulletActiveClass: 'slider__bullet--active',
+    },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.slider__btn--next',
+      prevEl: '.slider__btn--prev',
+    },
+    on: {
+      init() {
+        updateButtons(this);
+      },
+      slideChange() {
+        updateButtons(this);
+      },
     },
     breakpoints: {
-      // Адаптив
-      320: { slidesPerView: 1, slidesPerGroup: 1 },
+      300: { slidesPerView: 1, slidesPerGroup: 1 },
       768: { slidesPerView: 2, slidesPerGroup: 2 },
       1200: { slidesPerView: 3, slidesPerGroup: 3 },
     },
   });
 }
-*/
-import Swiper from 'swiper/bundle';
-import 'swiper/css';
-const swiperEl = document.querySelector('.swiper-wrapper');
-var swiper = new Swiper('.mySwiper', {
-  pagination: {
-    el: '.swiper-pagination',
-    dynamicBullets: true,
-  },
-});
+function updateButtons(swiper) {
+  const prevBtn = document.querySelector('.slider__btn--prev');
+  const nextBtn = document.querySelector('.slider__btn--next');
+  prevBtn.disabled = swiper.isBeginning;
+  nextBtn.disabled = swiper.isEnd;
+}
+
+function a(slidesData) {
+  return slidesData
+    .map(slide => {
+      const images = Array.isArray(slide.images)
+        ? slide.images
+        : [slide.images];
+      const colors = Array.isArray(slide.color) ? slide.color : [slide.color];
+      return images
+        .map((nameItem, index) => {
+          return `
+            <li class="slider__slide swiper-slide">
+              <img class="slider__image" src="${nameItem}" alt="${
+            slide.name
+          }" loading="lazy">
+              <h3 class="slider__title">${slide.name}</h3>
+              <h3 class="slider__price">${slide.price}</h3>
+               <ul class="slider__colors">
+              ${colors
+                .map(
+                  color =>
+                    `<li class="slider__color" style="background-color: ${color}"></li>`
+                )
+                .join('')}
+              </ul>
+          `;
+        })
+        .join('');
+    })
+    .join('');
+}
